a77eb72e95f088027e77237744c8d0d36d2f0a7c
Vũ Minh Quang <vumhquangg@gmail.com>
2024-11-23 07:49:19 +0700

    up source

---
+++ js/diagtialcv.js
@@ -0,0 +1,161 @@
+'use strict';
+
+addEventListener("DOMContentLoaded", (event) => {
+    
+    var dataValue = location.href.split("data_value=")[1];
+    if(dataValue == "project_01"){
+        showDetail(1);
+    } else if(dataValue == "project_02"){
+        showDetail(2);
+    } else {
+        showDetail(3);
+    }
+});
+
+function showDetail(numberProject){
+    if(numberProject == 1){
+
+        var _project = createObj(1);
+        
+        document.getElementById('goal_01').innerHTML = _project.goals[0];
+        document.getElementById('goal_02').innerHTML = _project.goals[1];
+        document.getElementById('goal_03').innerHTML = _project.goals[2];
+        document.getElementById('goal_04').innerHTML = _project.goals[1];
+        document.getElementById('goal_05').innerHTML = _project.goals[2];
+
+        document.getElementById('name_01').innerHTML = _project.teams[0];
+        document.getElementById('name_02').parentNode.parentNode.style.display = "none";
+        document.getElementById('name_03').parentNode.parentNode.style.display = "none";
+        document.getElementById('name_04').parentNode.parentNode.style.display = "none";
+
+
+        document.getElementById('scope_01').innerHTML = _project.scopes[0];
+        document.getElementById('scope_02').innerHTML = _project.scopes[1];
+
+        document.getElementById('primary_01').innerHTML = _project.primarys[0];
+        document.getElementById('primary_02').innerHTML = _project.primarys[1];
+
+        document.getElementById('timeline_01').innerHTML = _project.timelines[0];
+        document.getElementById('timeline_02').innerHTML = _project.timelines[1];
+        document.getElementById('timeline_03').innerHTML = _project.timelines[3];
+        document.getElementById('timeline_04').innerHTML = _project.timelines[4];
+        document.getElementById('timeline_05').innerHTML = _project.timelines[5];
+        document.getElementById('timeline_06').parentNode.parentNode.style.display = "none";
+        document.getElementById('timeline_07').parentNode.parentNode.style.display = "none";
+
+    } else if(numberProject == 2){
+
+        var _project = createObj(2);
+        
+        document.getElementById('goal_01').innerHTML = _project.goals[0];
+        document.getElementById('goal_02').innerHTML = _project.goals[1];
+        document.getElementById('goal_03').innerHTML = _project.goals[2];
+        document.getElementById('goal_04').innerHTML = _project.goals[1];
+        document.getElementById('goal_05').innerHTML = _project.goals[2];
+
+        document.getElementById('name_01').innerHTML = _project.teams[0];
+        document.getElementById('name_02').innerHTML = _project.teams[1];
+        document.getElementById('name_03').parentNode.parentNode.style.display = "none";
+        document.getElementById('name_04').parentNode.parentNode.style.display = "none";
+
+
+        document.getElementById('scope_01').innerHTML = _project.scopes[0];
+        document.getElementById('scope_02').innerHTML = _project.scopes[1];
+
+        document.getElementById('primary_01').innerHTML = _project.primarys[0];
+        document.getElementById('primary_02').innerHTML = _project.primarys[1];
+
+        document.getElementById('timeline_01').innerHTML = _project.timelines[0];
+        document.getElementById('timeline_02').innerHTML = _project.timelines[1];
+        document.getElementById('timeline_03').innerHTML = _project.timelines[3];
+        document.getElementById('timeline_04').innerHTML = _project.timelines[4];
+        document.getElementById('timeline_05').innerHTML = _project.timelines[5];
+        document.getElementById('timeline_06').parentNode.parentNode.style.display = "none";
+        document.getElementById('timeline_07').parentNode.parentNode.style.display = "none";
+    } else {
+
+        var _project = createObj(3);
+        
+        document.getElementById('goal_01').innerHTML = _project.goals[0];
+        document.getElementById('goal_02').innerHTML = _project.goals[1];
+        document.getElementById('goal_03').innerHTML = _project.goals[2];
+        document.getElementById('goal_04').innerHTML = _project.goals[1];
+        document.getElementById('goal_05').innerHTML = _project.goals[2];
+
+        document.getElementById('name_01').innerHTML = _project.teams[0];
+        document.getElementById('name_02').innerHTML = _project.teams[1];
+        document.getElementById('name_03').innerHTML = _project.teams[2];
+        document.getElementById('name_04').parentNode.parentNode.style.display = "none";
+
+
+        document.getElementById('scope_01').innerHTML = _project.scopes[0];
+        document.getElementById('scope_02').innerHTML = _project.scopes[1];
+
+        document.getElementById('primary_01').innerHTML = _project.primarys[0];
+        document.getElementById('primary_02').innerHTML = _project.primarys[1];
+
+        document.getElementById('timeline_01').innerHTML = _project.timelines[0];
+        document.getElementById('timeline_02').innerHTML = _project.timelines[1];
+        document.getElementById('timeline_03').innerHTML = _project.timelines[3];
+        document.getElementById('timeline_04').innerHTML = _project.timelines[4];
+        document.getElementById('timeline_05').innerHTML = _project.timelines[5];
+        document.getElementById('timeline_06').parentNode.parentNode.style.display = "none";
+        document.getElementById('timeline_07').parentNode.parentNode.style.display = "none";
+    }
+}
+
+function createObj(numberProject){
+    var Project = {
+        goals : [],
+        teams : [],
+        scopes : [],
+        primarys : [],
+        timelines : []
+    };
+
+    if (numberProject == 1){
+
+        Project.goals.push("Thiết kế web Responsive", "Hiển thị phần Thông tin cơ bản theo từng loại kích thước màn hình",
+        "Chức năng ẩn thông tin cá nhân", "Bổ sung Project Charter cho các dự án","Đưa trang Web lên online");
+        Project.teams.push("Name: Vu Minh Quang");
+        Project.scopes.push("scopes1", "scopes2");
+        Project.primarys.push("Hiển thị phần Thông tin cơ bản theo từng loại kích thước màn hình", "Bổ sung Project Charter cho các dự án");
+        Project.timelines.push("24/10 : Khoi chay du an", 
+        "24/10 - 30/11 : Hoan tat hien thi cac thong tin co ban" , 
+        "30/11 - 06/11 : Hoan thien hien thi man hinh theo tung kich thuoc" , 
+        "06/11 - 12/11 : Hoan tat chuc nang an thong tin" , 
+        "12/11 - 25/11 : Hoan tat bổ sung Project Charter cho các dự án " , 
+        "25/11 - 30/11 : Đưa trang Web lên online ");
+
+    } else if (numberProject == 2){
+
+        Project.goals.push("Thiết kế web Responsive", "Hiển thị phần Thông tin cơ bản theo từng loại kích thước màn hình",
+        "Hien thi thong tin danh sach cac thu cung","Chức năng thêm, xoá, sửa","Đưa trang Web lên online");
+        Project.teams.push("Name: Vu Minh Quang","Name: Vu Nhat Hung");
+        Project.scopes.push("scopes1", "scopes2");
+        Project.primarys.push("Hiển thị phần Thông tin cơ bản theo từng loại kích thước màn hình", "Chức năng thêm, xoá, sửa");
+        Project.timelines.push("24/10 : Khoi chay du an", 
+        "24/10 - 30/11 : Hoan tat hien thi cac thong tin co ban" , 
+        "30/11 - 06/11 : Hoan thien hien thi man hinh theo tung kich thuoc" , 
+        "06/11 - 12/11 : Hien thi thong tin danh sach cac thu cung" , 
+        "12/11 - 25/11 : Chức năng thêm, xoá, sửa " , 
+        "25/11 - 30/11 : Đưa trang Web lên online ");
+
+    } else {
+
+        Project.goals.push("Thiết kế web Responsive", "Hiển thị phần Thông tin cơ bản theo từng loại kích thước màn hình",
+        "Hien thi thong tin danh sach cac tin tuc","Chức năng thêm, xoá, sửa tin tuc","Đưa trang Web lên online");
+        Project.teams.push("Name: Vu Minh Quang","Name: Vu Nhat Hung","Name: Vu Nhat Hieu");
+        Project.scopes.push("scopes1", "scopes2");
+        Project.primarys.push("Hiển thị phần Thông tin cơ bản theo từng loại kích thước màn hình", "Chức năng thêm, xoá, sửa tin tuc");
+        Project.timelines.push("24/10 : Khoi chay du an", 
+        "24/10 - 30/11 : Hoan tat hien thi cac thong tin co ban" , 
+        "30/11 - 06/11 : Hoan thien hien thi man hinh theo tung kich thuoc" , 
+        "06/11 - 12/11 : Hien thi thong tin danh sach cac tin tuc" , 
+        "12/11 - 25/11 : Chức năng thêm, xoá, sửa tin tuc" , 
+        "25/11 - 30/11 : Đưa trang Web lên online ");
+
+    };
+    
+    return Project;   
+}
---
+++ js/script.js
@@ -0,0 +1,115 @@
+'use strict';
+
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById('info').style.display = "none";
+    document.getElementById('experience').style.display = "none";
+    document.getElementById('education').style.display = "none";
+    document.getElementById('work').style.display = "none";
+    document.getElementById('hobby').style.display = "none";
+    document.getElementById('language').style.display = "none";
+    document.getElementById('skill').style.display = "none";
+});
+
+document.getElementById("lemail").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        handleChange();
+    }
+});
+
+
+function handleChange() {
+    let valueOfChange = document.getElementById('lemail').value;
+    if(regex.test(valueOfChange)){
+        document.getElementById('confirmInfo').style.display = "none";
+        document.getElementById('info').style.display = "";    
+    } else {
+        document.getElementById('confirmInfo').style.display = "";
+        document.getElementById('info').style.display = "none";
+        alert("Nhap sai vui long nhap lai");
+    }
+
+}
+
+var exp = document.getElementById("experience");
+var education = document.getElementById("education");
+var work = document.getElementById("work");
+var hobby = document.getElementById("hobby");
+var language = document.getElementById("language");
+var skill = document.getElementById("skill");
+
+function handleShowS(classStr) {
+    if(classStr == "item1" && exp.style.display == "none"){
+        exp.style.display = "";
+        document.getElementById('btnExperienceS').style.display = "none";
+        document.getElementById('btnExperienceH').style.display = "block";
+    } else if (classStr == "item3" && education.style.display == "none") {
+        education.style.display = "";
+        document.getElementById('btnEducationS').style.display = "none";
+        document.getElementById('btnEducationH').style.display = "block";
+    } else if (classStr == "item2" && work.style.display == "none") {
+        work.style.display = "";
+        document.getElementById('btnWorkS').style.display = "none";
+        document.getElementById('btnWorkH').style.display = "block";
+    } else if (classStr == "item5" && hobby.style.display == "none") {
+        hobby.style.display = "";
+        document.getElementById('btnHobbyS').style.display = "none";
+        document.getElementById('btnHobbyH').style.display = "block";
+    } else if (classStr == "item6" && language.style.display == "none") {
+        language.style.display = "";
+        document.getElementById('btnLanguageS').style.display = "none";
+        document.getElementById('btnLanguageH').style.display = "block";
+    } else if (classStr == "item4" && skill.style.display == "none") {
+        skill.style.display = "";
+        document.getElementById('btnSkillS').style.display = "none";
+        document.getElementById('btnSkillH').style.display = "block";
+    }
+    
+}
+
+function handleShowH(classStr) { 
+    if(classStr == "item1" && exp.style.display == ""){
+        exp.style.display = "none";
+        document.getElementById('btnExperienceS').style.display = "";
+        document.getElementById('btnExperienceH').style.display = "none";
+    } else if (classStr == "item3" && education.style.display == "") {
+        education.style.display = "none";
+        document.getElementById('btnEducationS').style.display = "";
+        document.getElementById('btnEducationH').style.display = "none";
+    } else if (classStr == "item2" && work.style.display == "") {
+        work.style.display = "none";
+        document.getElementById('btnWorkS').style.display = "";
+        document.getElementById('btnWorkH').style.display = "none";
+    } else if (classStr == "item5" && hobby.style.display == "") {
+        hobby.style.display = "none";
+        document.getElementById('btnHobbyS').style.display = "";
+        document.getElementById('btnHobbyH').style.display = "none";
+    } else if (classStr == "item6" && language.style.display == "") {
+        language.style.display = "none";
+        document.getElementById('btnLanguageS').style.display = "";
+        document.getElementById('btnLanguageH').style.display = "none";
+    } else if (classStr == "item4" && skill.style.display == "") {
+        skill.style.display = "none";
+        document.getElementById('btnSkillS').style.display = "";
+        document.getElementById('btnSkillH').style.display = "none";
+    }
+}
+
+const project_1 = document.getElementById('project_1');
+const project_2 = document.getElementById('project_2');
+const project_3 = document.getElementById('project_3');
+
+project_1.addEventListener("click", function(){
+    window.location="/digital-cv.html?data_value=project_01";
+});
+
+project_2.addEventListener("click", function(){
+    window.location="/digital-cv.html?data_value=project_02";
+});
+
+project_3.addEventListener("click", function(){
+    window.location="/digital-cv.html?data_value=project_03";
+});
+
